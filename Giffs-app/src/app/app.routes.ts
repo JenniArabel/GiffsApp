4fd3575d5671194491817c13bab09cc0ff34866e
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-03 12:52:39 -0300

    Working in: child routes, RouterLink, Components from Menu side

--- Giffs-app/src/app/app.routes.ts
+++ Giffs-app/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./giffs/pages/dashboard-page/dashboard-page.component'),
+    children: [
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./giffs/pages/search-page/search-page.component'),
+      },
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./giffs/pages/trending-page/trending-page.component'),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
---
+++ Giffs-app/src/app/giffs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  sublabel: string;
+  router: string;
+}
+
+@Component({
+  selector: 'app-gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gifs-side-menu-options.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GifsSideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs populares',
+      router: '/dashboard/trending',
+    },
+    {
+      icon: 'search',
+      label: 'Buscar',
+      sublabel: 'Buscar gifs',
+      router: '/dashboard/search',
+    },
+  ];
+}
