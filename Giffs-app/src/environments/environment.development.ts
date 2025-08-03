30f89f6850e3c674d0e57f0b9206e799b40ecdb2
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-03 13:03:03 -0300

    Working with environments in por Menu Header.

--- Giffs-app/src/environments/environment.development.ts
+++ Giffs-app/src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  productuion: true,
+  companyName: 'Giffs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+  //Api Keys
+
+  //URLS
+};
